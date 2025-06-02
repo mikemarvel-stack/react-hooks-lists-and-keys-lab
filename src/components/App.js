18890c1cc35a313457580e38797e69629a7f6886
michael <michaelmuturi15@gmail.com>
2025-06-02 18:36:21 +0300

    components updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,35 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
 
 function App() {
+  const projects = [
+    {
+      id: 1,
+      name: "Reciplease",
+      about: "A recipe tracking app",
+      technologies: ["Rails", "Bootstrap CSS"],
+    },
+    {
+      id: 2,
+      name: "Kibbles N Bitz",
+      about: "Tinder for dogs",
+      technologies: ["React", "Redux"],
+    },
+    {
+      id: 3,
+      name: "Alienwares",
+      about: "Etsy for aliens",
+      technologies: ["React", "Redux", "Rails"],
+    },
+  ];
+
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projects} />
     </div>
   );
 }
